72dafc1fa39888e8005505fa7e0bc4c2ff4ca57e
MariusLastauskas <marius20122@gmail.com>
2018-03-01 21:37:28 +0200

    Castle buildin in tiles
    So far just the begginning

---
+++ Castle seage/Assets/Scripts/WallPlacer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallPlacer : MonoBehaviour {
+
+    private Grid grid;
+
+	// Use this for initialization
+	void Start () {
+        grid = FindObjectOfType<Grid>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                placeBuildingNear(hitInfo.point);
+            }
+        }
+	}
+
+    private void placeBuildingNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+    }
+}
