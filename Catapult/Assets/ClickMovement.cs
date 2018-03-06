7e8c4d8d05c99c9ec46ef6954dee8c956aaf97cb
Clark-Ken <36919527+Clark-Ken@users.noreply.github.com>
2018-03-06 16:39:36 +0200

    Judėjimas2
    Padariau, kad kunas judėtu, ten kur paspaudžiama su pelyte.

---
+++ Catapult/Assets/ClickMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickMovement : MonoBehaviour {
+
+    public LayerMask clickableArea;
+
+    private NavMeshAgent myAgent;
+	
+    // Use this for initialization
+	void Start () {
+        myAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(myRay, out hitInfo, 100, clickableArea))
+            {
+                myAgent.SetDestination(hitInfo.point);
+            }
+        }
+	}
+}
