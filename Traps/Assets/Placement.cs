e016b0933e25398facebb01d573031985920b8b9
Asyde <36919483+Asyde@users.noreply.github.com>
2018-03-06 11:04:07 +0200

    Tiles
    Sukuriau "tiles", ant kurių vėliau bus dedami spastai.

---
+++ Traps/Assets/Placement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placement : MonoBehaviour
+{
+    private Grids grid;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grids>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+    }
+}
