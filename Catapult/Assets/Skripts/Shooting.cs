a47424ac507bc36919a8abbcb297dc50a3d199ca
Clark-Ken <36919527+Clark-Ken@users.noreply.github.com>
2018-03-12 17:20:16 +0200

    SH11+GAM7
    Parašiau kodą, kurį galima pritaikyti visiems unitams, kurie šaudo iš toli. Dar reikia sudėti animacijas ir spritus. Sukuriau primitivų modelį katapultos/žmogeliuko.

---
+++ Catapult/Assets/Skripts/Shooting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public bool isFiring;
+
+    public Projectile projectile;
+    public float projectileSpeed;
+
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    public Transform spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isFiring)
+        {
+            shotCounter -= Time.deltaTime;
+            if (shotCounter <= 0)
+            {
+                shotCounter = timeBetweenShots;
+                Projectile newProject = (Projectile)Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
+                newProject.speed = projectileSpeed;
+            }
+        }
+        else
+        {
+            shotCounter = 0;
+        }
+	}
+}
