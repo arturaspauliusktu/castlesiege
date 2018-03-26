c43a9528280af9796295ba1a97cfc54fbd1b2b7c
Clark-Ken <36919527+Clark-Ken@users.noreply.github.com>
2018-03-26 17:20:20 +0300

    AIS12
    Ekrane pasirodo Defeat!, kai įvykdoma tam tikras veiksmas.

---
+++ Castle siege/Assets/Skripts/GameEndManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndManager : MonoBehaviour {
+
+    public float restarDelay = 5;
+    Animator anim;
+    float timer;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("p"))
+        {
+            anim.SetTrigger("GameEnd");
+
+            timer += Time.deltaTime;
+            if (timer >= restarDelay)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+	}
+}
