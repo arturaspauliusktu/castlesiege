819c053d2aa0a335cdc8fb215ffad0746ef51c80
Artūras Paulius <arturas.paulius@gmail.com>
2018-03-31 23:54:33 +0300

    AIS7 - Naujas Branch, Paprastas unito vaiksciojimas per mapa.
    1. Pauspaudus pelyte ant mapo unitas eina link pazimetos vietos.
    2. Idetas assetas NavMeshComponent, kuris kuria meshus kur galima vaikscioti.
    3. Geltonas kubas yra karalius :D

--- Castle siege/Assets/Scripts/Unit_Scripts/Unit.cs
+++ Castle siege/Assets/Scripts/Unit_Scripts/Unit.cs
@@ -1,25 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class Unit : MonoBehaviour {
 
     public int MaxUnitHealth = 100;
     public int UnitHealth = 100;
+    public NavMeshAgent agent;
+    public Camera cam;
     bool isAlive = true;
     Unit_Health health;
     Break br;
+    GameObject King;
+    
 
     // Use this for initialization
     void Start () {
         health = new Unit_Health(MaxUnitHealth, UnitHealth);
         br = gameObject.GetComponent<Break>();
+        King = GameObject.FindWithTag("King");
+        Vector3 pos = King.transform.position;
+        pos.y = 0;
+        agent.SetDestination(pos);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
 
-        if(collision.relativeVelocity.magnitude > 10.0f)
+        if(collision.relativeVelocity.magnitude > 50.0f)
         {
             Debug.Log(health.getHealth());
 
@@ -34,7 +41,17 @@ public class Unit : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update() {
         UnitHealth = health.getHealth();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
 	}
 }
