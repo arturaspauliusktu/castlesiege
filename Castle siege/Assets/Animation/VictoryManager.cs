4d088a5fc10aaaae715811216fcbd643785e7788
Clark-Ken <36919527+Clark-Ken@users.noreply.github.com>
2018-04-23 14:09:01 +0300

    UI211-victory clip(animacija) sukurta

---
+++ Castle siege/Assets/Animation/VictoryManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryManager : MonoBehaviour {
+
+    public float restartDelay = 5;
+    Animator anim;
+    float restarTimer;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("v"))
+        {
+            anim.SetTrigger("Victory");
+
+            restarTimer += Time.deltaTime;
+
+            if(restarTimer >= restartDelay)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }	
+    }
+}
