0ac767a3cb4a3145c03548b14539488f4973ef95
Artūras Paulius <arturas.paulius@gmail.com>
2018-04-24 16:20:11 +0300

    SH12 - Archer unitas
    Archer unitas dabar saudo kai aptinka priesa

--- Castle siege/Assets/Scripts/FlyingArrow.cs
+++ Castle siege/Assets/Scripts/FlyingArrow.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class FlyingArrow : MonoBehaviour {
 
+    public int damage;
     private Rigidbody rb;
+    private bool isHit = false;
+    private Transform anchor;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +16,38 @@ public class FlyingArrow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.rotation = Quaternion.LookRotation(rb.velocity);
-	}
+        if(!this.isHit)
+            transform.rotation = Quaternion.LookRotation(rb.velocity);
+        else if (this.anchor != null)
+        {
+            this.transform.position = anchor.transform.position;
+            this.transform.rotation = anchor.transform.rotation;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!isHit)
+        {
+            ArrowStick(collision);
+            this.isHit = true;
+            Unit unit = collision.gameObject.GetComponent<Unit>();
+            if(!Unit.IsDeadOrNull(unit))
+                unit.SufferAttack(damage);
+        }
+    }
+
+    private void ArrowStick(Collision other)
+    {
+        this.transform.position = other.contacts[0].point;
+        this.GetComponent<Collider>().isTrigger = true;
+
+        GameObject anchor = new GameObject("ARROW_ANCHOR");
+        anchor.transform.position = this.transform.position;
+        anchor.transform.rotation = this.transform.rotation;
+        anchor.transform.parent = other.transform;
+        this.anchor = anchor.transform;
+
+        Destroy(rb);
+    }
 }
