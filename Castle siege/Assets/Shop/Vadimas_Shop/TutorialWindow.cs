8f829da6f93af6ea260c77388a55fa9fd9f0647e
Clark-Ken <36919527+Clark-Ken@users.noreply.github.com>
2018-05-21 14:16:03 +0300

    UI-Fixes: Idetas tutorial langas ir mygtukas. Padarytas, kad shop sutaptu su ekrano rezoliucija. Tutorial mygtukas nevisai veikia.

---
+++ Castle siege/Assets/Shop/Vadimas_Shop/TutorialWindow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialWindow : MonoBehaviour {
+
+    public bool isImageOn;
+    public GameObject tutorialWndow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ChooseOnOff();
+	}
+
+    public void ChooseOnOff()
+    {
+        if (isImageOn)
+        {
+            ShowTutorialWindow();
+        }
+        else
+        {
+            HideTutorialWindow();
+        }	
+    }
+
+    public void ShowTutorialWindow()
+    {
+        isImageOn = true;
+        tutorialWndow.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void HideTutorialWindow()
+    {
+        isImageOn = false;
+        tutorialWndow.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
